f5ea31d2d3979154092cfdb9f550a7d17ec38400
AgustinCappellacci <86740433+AgustinCappellacci@users.noreply.github.com>
2022-04-05 11:29:44 -0300

    Add files via upload

---
+++ problem9.js
@@ -0,0 +1,24 @@
+/* 
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward.
+*/
+
+//Function
+const isPalindrome = (num) => {
+    let numString = String(num)
+    let reverse = ''
+    for (let i = numString.length-1; i >= 0; i--) {
+        reverse+=numString[i]  
+    }
+    if (numString==reverse){
+        console.log(`${num} is a palindrome`)
+        return
+    }
+    console.log(`${num} is NOT a palindrome`)
+}
+
+//Test
+
+isPalindrome(231);
+isPalindrome(93739);
